e9d1bc1caaaff2f6bb90aa4138a383cb6f126b2d
cuda lab12 <cuda-s12@nat110-fix-194.fis.agh.edu.pl>
2019-10-21 13:59:56 +0200

    Testing performance of program vectorAdd. Bash script to do it faster.

--- Lab01/vectorAdd_performance/src/vectorAdd_performance.cu
+++ Lab01/vectorAdd_performance/src/vectorAdd_performance.cu
@@ -50,7 +50,7 @@ main(void)
     cudaError_t err = cudaSuccess;
 
     // Print the vector length to be used, and compute its size
-    int numElements = 50000;
+    int numElements = 1000000000;
     size_t size = numElements * sizeof(float);
     printf("[Vector addition of %d elements]\n", numElements);
 
